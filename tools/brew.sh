324eaddffe717846e7eb044d19450676ff251058
Alok Choudhary <alok.c83@gmail.com>
2021-08-16 07:06:54 +0000

    brew install script

---
+++ tools/brew.sh
@@ -0,0 +1,5 @@
+sudo apt update;
+sudo apt-get install build-essential;
+sudo apt install git -y;
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+#eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
